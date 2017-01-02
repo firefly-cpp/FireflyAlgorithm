170394dc2648db570d75873af470157dd2c18c25
firefly-cpp <pingvin1@gmail.com>
2017-01-02 11:38:04 +0100

    First commit

---
+++ run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+from FireflyAlgorithm import *
+
+
+def Fun(D, sol):
+    val = 0.0
+    for i in range(D):
+        val = val + sol[i] * sol[i]
+    return val
+
+
+Algorithm = FireflyAlgorithm(10, 20, 10000, 0.5, 0.2, 1.0, -2.0, 2.0, Fun)
+Best = Algorithm.Run()
+
+print Best
