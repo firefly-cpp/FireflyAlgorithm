75ded5f52d1afec3733a948729a8a0824f7a555a
firefly-cpp <pingvin1@gmail.com>
2019-06-11 22:37:54 +0200

    setup file added

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# FireflyAlgorithm
+# Firefly Algorithm in Python
+
+## CODE EXAMPLE:
+
+```python
+from FireflyAlgorithm import *
+
+
+def Fun(D, sol):
+    val = 0.0
+    for i in range(D):
+        val = val + sol[i] * sol[i]
+    return val
+
+
+Algorithm = FireflyAlgorithm(10, 20, 10000, 0.5, 0.2, 1.0, -2.0, 2.0, Fun)
+Best = Algorithm.Run()
+
+print Best
+```
+
+## Bugs
+Bugs and extension should be send via Github.
+
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name='FireflyAlgorithm',
+      description='Firefly algorithm implementation',
+      author='firefly-cpp',
+      version='0.0.1',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development'
+      ],
+      url='https://github.com/firefly-cpp/FireflyAlgorithm',
+      py_modules=['FireflyAlgorithm']
+      ) 
+
