8c6a32ad0b7e074e661f4a8415353674ae201064
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-03-12 15:55:04 +0100

    Interface changes

--- examples/run.py
+++ examples/run.py
@@ -6,7 +6,7 @@ def sphere(x):
     return np.sum(x ** 2)
 
 
-FA = FireflyAlgorithm(function=sphere, dim=10, lb=-5, ub=5, max_evals=10000)
-best = FA.move()
+FA = FireflyAlgorithm()
+best = FA.run(function=sphere, dim=10, lb=-5, ub=5, max_evals=10000)
 
 print(best)
